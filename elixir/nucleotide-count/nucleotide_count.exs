49d67e72132345bf29ecec0f375e030bb5553398
LuisSas <luis@makingdevs.com>
2019-01-13 00:12:25 -0600

    Passing the first 3 test for nucleotide exercise

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -14,6 +14,14 @@ defmodule NucleotideCount do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
+    count_letters( strand, nucleotide, 0 )
+  end
+
+  defp count_letters( [], _nucleotide, number), do: number
+  defp count_letters( strand, nucleotide, number) do
+    [ comparate_letter | rest ] = strand
+    number = if (comparate_letter == nucleotide) do number + 1 else number end
+    count_letters( rest, nucleotide, number )
   end
 
   @doc """
